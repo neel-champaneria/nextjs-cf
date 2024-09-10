97bc5e5908ae7f4230d5201643ac139a75792652
Neel Champaneria <neel@promactinfo.com>
2024-09-11 00:39:50 +0530

    Add prisma and api

--- app/api/thoughts/route.ts
+++ app/api/thoughts/route.ts
@@ -1,5 +1,20 @@
+import prisma from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
 export const runtime = "edge";
 
+export async function POST(request: NextRequest) {
+  const { text } = await request.json();
+  const thought = await prisma.thought.create({
+    data: { text },
+  });
+  return Response.json(
+    { message: "Thought Created", thought },
+    { status: 201 }
+  );
+}
+
 export async function GET() {
-  return Response.json({ message: "Hello" });
+  const thoughts = await prisma.thought.findMany();
+  return Response.json({ thoughts });
 }
